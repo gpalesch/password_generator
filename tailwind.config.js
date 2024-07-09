da8cf829da59c1da90e4d9d62521f6ad67c970c3
Gina Palesch <gpalesch@bynativ.com>
2024-07-09 20:03:49 +0200

    moficications on devices up to lg (not complete)

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,10 @@ module.exports = {
         'clamp-31-51': 'clamp(31.75px, 9.95vw, 51px)',
         'clamp-10-16': 'clamp(10.5px, 3.3vw, 16px)',
         'clamp-12-20': 'clamp(12px, 3.8vw, 20px)',
+        '38': '38px',
+        '40': '40px',
+        '42': '42px',
+        '52': '52px',
       },
       height: {
         '6vw': '6vw',
@@ -27,12 +31,14 @@ module.exports = {
         '14vw': '14vw',
         'clamp-14-24': 'clamp(14px, 4.4vw, 24.8px)',
         'clamp-8-14': 'clamp(8px, 2.6vw, 14px)',
+        'custom-51': '51px',
       },
       width: {
         '14.85vw': '14.85vw',
       },
       maxWidth: {
-        'custom-410': '410px'
+        'custom-410': '410px',
+        'custom-820': '820px',
       },
       margin: {
         'clamp-27-60': 'clamp(27px, 8.05vw, 60px)',
