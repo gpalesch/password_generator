9e0b97a895ef395dc77d38f1fd422851a2748d79
Gina Palesch <gpalesch@bynativ.com>
2024-07-09 13:15:25 +0200

    modifications on generate_button for mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,14 @@ module.exports = {
         'clamp-31-51': 'clamp(31.75px, 9.95vw, 51px)',
       },
       height: {
-        '6.6vw': '6.6vw',
+        '6vw': '6vw',
         '8vw': '8vw',
         '9.5vw': '9.5vw',
         '14vw': '14vw',
+        'clamp-14-24': 'clamp(14px, 4.4vw, 24.8px)',
+      },
+      width: {
+        '14.85vw': '14.85vw',
       },
       maxWidth: {
         'custom-410': '410px'
@@ -31,8 +35,7 @@ module.exports = {
         'clamp-27-60': 'clamp(27px, 8.05vw, 60px)',
         'clamp-10-20': 'clamp(10px, 3.8vw, 20px)',
         'clamp-4-6': 'clamp(4px, 1.15vw, 6px)',
-        'clamp-11-25': 'clamp(11.5px, 4.75vw, 25px)',
-        'clamp-17-50': 'clamp(17px, 5w, 50px)',
+        'clamp-11-30': 'clamp(11.5px, 4.75vw, 30px)',
       },
     },
   },
